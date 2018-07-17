fe5789d8acf71328c971ad79e13aa09cc6d3b340
casselsg <casselsg19@bcdschool.org>
2018-07-17 15:04:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "It was so awesome to see Karlie yesterday! She put a lot of our photos on her instagram!"
   end
 
+get '/kitty' do 
+  "Hello Kitty!"
 end
+
+get '/lolipop' do
+  "I like Icecream!"
+end
+
+get '/sausage'
+"say sausage or die"
+ 
+ end
